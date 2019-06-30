567d0dbd385bfe026c2700ee62756a10932d93e4
Vitas1968 <dor1968@mail.ru>
2019-06-30 22:28:10 +0700

    Добавлен метод рисования окна

--- src/TestCodeMain.java
+++ src/TestCodeMain.java
@@ -1,5 +1,22 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
 public class TestCodeMain
 {
+    public static void main(String[] args)
+    {
+        createGUI();
+
+    }
+
+    public static void createGUI()
+    {
+        JFrame frame = new JFrame("Test frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Test label");
+        frame.getContentPane().add(label);
+        frame.setPreferredSize(new Dimension(200, 100));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
